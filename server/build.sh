11bf30eea86ca5eed29546d8412059cec1187020
Ryan Weast <ryweast@gmail.com>
2025-10-07 09:04:04 -0400

    Fix build.sh (#23)

--- server/build.sh
+++ server/build.sh
@@ -24,6 +24,9 @@ export PROMETHEUS_USER=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME/prom
 export PROMETHEUS_PASSWORD=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME/prometheus_pw" --with-decryption --query "Parameter.Value" --output text)
 htpasswd -cBb ./docker/nginx/.htpasswd.loki "$LOKI_USER" "$LOKI_PASSWORD"
 htpasswd -cBb ./docker/nginx/.htpasswd.prometheus "$PROMETHEUS_USER" "$PROMETHEUS_PASSWORD"
+
+# Run all these commands as ec2-user (required because it establishes new docker group)
+sudo -u ec2-user --preserve-env=APP_NAME,ENV_NAME -i <<'EOF'
 # Get Grafana parameters
 # DB
 export GF_DATABASE_HOST=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME/rds_host" --query "Parameter.Value" --output text)
@@ -33,14 +36,12 @@ export GF_DATABASE_PASSWORD=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME
 # DNS
 export GF_SERVER_DOMAIN=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME/domain_name" --query "Parameter.Value" --output text)
 export GF_SERVER_ROOT_URL="https://$GF_SERVER_DOMAIN/"
+# -i logs us in as shell so our pwd gets reset
+cd ~/grafana-iac/server
 # Update loki-config.yaml with correct s3 bucket name
 export LOKI_S3_BUCKET=$(aws ssm get-parameter --name "/$APP_NAME/$ENV_NAME/grafana_s3_name" --query "Parameter.Value" --output text)
 envsubst <docker/loki/loki-config-template.yaml >docker/loki/loki-config.yaml
 envsubst <docker/mimir/mimir-template.yaml >docker/mimir/mimir.yaml
-# Run all these commands as ec2-user (required because it establishes new docker group)
-sudo -u ec2-user -E -i <<'EOF'
-# -i logs us in as shell so our pwd gets reset
-cd ~/grafana-iac/server
 # Now, docker compose
 docker-compose -f docker/docker-compose.yaml up -d
 EOF
